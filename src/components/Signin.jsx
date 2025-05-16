2a95ff7700889964ce59472fb9a564c8efe6804c
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-05-16 17:40:25 +0600

    Delete, Get ,Post done

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
 const Signin = () => {
+
+    const handleSignin=(e)=>{
+          e.preventDefault()
+    }
     return (
-        <div>
-            Sign in
-        </div>
+         <div className="min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-md">
+        <form onClick={handleSignin} className="fieldset p-6 border rounded-lg shadow-md">
+
+          <label className="label">Email</label>
+          <input type="email" name='email' className="input input-bordered w-full mb-4" placeholder="Email" />
+
+          <label className="label">Password</label>
+          <input type="password" name='password' className="input input-bordered w-full mb-2" placeholder="Password" />
+
+          <div><a className="link link-hover text-sm">Forgot password?</a></div>
+
+          <button className="btn btn-neutral mt-4 w-full">Login</button>
+        </form>
+      </div>
+    </div>
     );
 };
 
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router';
+import Swal from 'sweetalert2';
 
 const User = () => {
-  const users = useLoaderData();
+  const initialUsers = useLoaderData();
+  const [users, setUsers] = useState(initialUsers)
+
+  const handleDelete = (id) => {
+    console.log('Handle Delete', id)
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        fetch(`http://localhost:3000/users/${id}`, {
+          method: 'DELETE'
+        })
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            if (data.deletedCount) {
+              
+              const remainUser = users.filter(user=>user._id !== id)
+              setUsers(remainUser)
+
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+
+          })
+
+
+
+      }
+    });
+  }
 
   return (
     <div className="p-4">
@@ -14,6 +56,7 @@ const User = () => {
             <th className="border border-gray-300 px-4 py-2">Address</th>
             <th className="border border-gray-300 px-4 py-2">Phone</th>
             <th className="border border-gray-300 px-4 py-2">Photo</th>
+            <th className="border border-gray-300 px-4 py-2">Others</th>
           </tr>
         </thead>
         <tbody>
@@ -25,6 +68,12 @@ const User = () => {
               <td className="border border-gray-300 px-4 py-2">{user.phone}</td>
               <td className="border border-gray-300 px-4 py-2">
                 <img src={user.Photo} alt={user.name} className="w-16 h-16 object-cover mx-auto" />
+              </td>
+
+              <td className=' px-4 py-2 flex justify-center items-center gap-2'>
+                <button className='btn'>View</button>
+                <button className='btn'>Edit</button>
+                <button onClick={() => handleDelete(user._id)} className='btn'>Details</button>
               </td>
             </tr>
           ))}
